5ee1268ed60961347bd6529be48d6d49692f2579
Sidhujhota <158768932+Sidhujhota@users.noreply.github.com>
2024-02-14 23:14:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "12227067"))
 API_HASH = getenv("API_HASH", "b463bedd791aa733ae2297e6520302fe")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "6204761408"))
+OWNER_ID = int(getenv("OWNER_ID", "6394947574"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "samuelsfriends")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "AbhiModszYT_Return")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "AM_YTBott")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "PUNJXB")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "sidhuchatx")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "i_am_champion")
 
 # Random Start Images
 IMG = [
